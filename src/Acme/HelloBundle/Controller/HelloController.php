82b7ae549e0e0420496beeeccad3358ab28ae8d1
nathaniel.r.wheeler <nathaniel.r.wheeler@gmail.com>
2014-01-29 19:32:59 -0500

    Routes now utilize annotations.

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -2,12 +2,14 @@
 
 namespace Acme\HelloBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HelloController extends Controller
 {
     /**
+     *@Route("/hello/{name}", name="hello")
      *@Template
      */
     public function indexAction($name)
